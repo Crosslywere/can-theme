8240502aeab46e05b02e4d06c4f72f473cc6b1f8
Crosslywere <71974578+Crosslywere@users.noreply.github.com>
2022-08-27 07:50:48 +0100

    archive page for "our work" added

---
+++ archive-our-work.php
@@ -0,0 +1,18 @@
+<?php get_header();?>
+<main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <?php $the_query = new WP_Query(['post_type'=>'misc', 'name'=>'our-work']);?>
+            <?php if($the_query->have_posts()): $the_query->the_post();?>
+                <div class="col col-md-9 col-lg-7">
+                    <h2 class="text-can-primary text-center mt-3 mb-4 font-black"><?php the_title();?></h2>
+                    <?php the_content();?>
+                </div>
+            <?php endif; wp_reset_postdata();?>
+        </div>
+        <div class="row px-0 my-5">
+            <?php get_template_part('includes/section', 'our-work');?>
+        </div>
+    </div>
+</main>
+<?php get_footer();?>
