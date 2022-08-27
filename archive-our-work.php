f1d29cac52a4fea5464697a3bb28b8cb86802035
Crosslywere <71974578+Crosslywere@users.noreply.github.com>
2022-08-27 08:23:56 +0100

    added "our work" section

--- archive-our-work.php
+++ archive-our-work.php
@@ -10,7 +10,7 @@
                 </div>
             <?php endif; wp_reset_postdata();?>
         </div>
-        <div class="row px-0 my-5">
+        <div class="row px-0 my-5 d-table">
             <?php get_template_part('includes/section', 'our-work');?>
         </div>
     </div>
---
+++ includes/section-our-work.php
@@ -0,0 +1,8 @@
+<?php if(have_posts()):while(have_posts()):the_post()?>
+<a href="<?php the_permalink();?>" class="text-decoration-none col-12 col-md-6 col-lg-4 px-md-0 d-table-cell" style="height:calc(100% * (600 / 534));background-image:url('<?php echo the_post_thumbnail_url()?>')">
+    <div class="p-4 mt-5">
+        <h1 class="text-white font-black"><?php the_title();?></h1>
+        <span class="text-white"><?php the_excerpt();?></span>
+    </div>
+</a>
+<?php endwhile; endif;?>
